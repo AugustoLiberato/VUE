7b7a48bafbc5b95e98621cdd9ebf64617bd13084
AugustoLiberato <augusto.liberato10@gmail.com>
2024-10-10 18:08:01 -0300

    diretivas

--- 2_projeto_vue/src/components/Info.vue
+++ 2_projeto_vue/src/components/Info.vue
@@ -1,20 +1,24 @@
 <template>
     <div>
         <p v-if="esta_trabalhando">Estou trabalhando no momento.</p>
+        <p v-else>Estou em busca de novas oportunidades</p>
         <p>Utilizo as seguintes tecnologias</p>
         <ul>
             <li>JavaScript</li>
             <li>PHP</li>
             <li>Python</li>
         </ul>
+        <p v-show="mostrar_email">Mande uma mensagem para: {{ email }}</p>
     </div>
 </template>
 <script>
     export default {
         name: 'Info',
-        Data(){
+        data(){
             return {
-                esta_trabalhando: true
+                esta_trabalhando: false,
+                mostrar_email: true,
+                email: 'email@gmail.com'
             }
         }
     }
