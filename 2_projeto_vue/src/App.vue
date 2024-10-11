c669f1b5a8987fe9f85dbcdbf3cfb626174f22e2
AugustoLiberato <augusto.liberato10@gmail.com>
2024-10-11 17:13:18 -0300

    Fui ate o 15

--- 2_projeto_vue/src/App.vue
+++ 2_projeto_vue/src/App.vue
@@ -1,12 +1,14 @@
 <template>
   <div>
+    <Header />
     <PrimeiroComponente/>
-    <p>O dado de app é: {{ testando }}</p>
-    <LifeCycle/>
-    <Pessoa/>
+    <p class="paragrafo-pai">O dado de app é: {{ testando }}</p>
+    <LifeCycle />
+    <Pessoa />
   </div>
 </template>
 <script>
+  import Header from './components/Header.vue'
   import PrimeiroComponente from './components/PrimeiroComponente.vue'
   import LifeCycle from './components/LifeCycle.vue'
   import Pessoa from './components/Pessoa.vue'
@@ -14,14 +16,30 @@
 export default {
   name: 'App',
   components: {
+    Header,
     PrimeiroComponente,
     LifeCycle,
     Pessoa
   },
-Data() {
+data() {
   return{
     testando: "Teste"
   }
 }
 }
 </script>
+
+<style>
+body {
+  background-color: #333;
+  color: #FFF;
+}
+a {
+  color: red;
+}
+
+.teste {
+  background-color: #000;
+}
+
+</style>
--- 2_projeto_vue/src/components/LifeCycle.vue
+++ 2_projeto_vue/src/components/LifeCycle.vue
@@ -14,13 +14,18 @@ export default {
         setTimeout(() => {
             this.nome = 'Augusto'
         }, 1000);
-        
+        this.lifeCycle()
     },
     mounted() {
         setTimeout(() => {
             this.nome = 'Pedro'
         }, 2000)
-        
+        this.lifeCycle()
+    },
+    methods: {
+        lifeCycle() {
+            console.log("Executou")
+        }
     }
 }
 </script>
